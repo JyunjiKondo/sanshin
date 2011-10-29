475da7c48668dca20ea25b92add5649b2f7b427c
Jyunji Kondo <jyukon@gmail.com>
2011-10-29 20:19:02 +0900

    select and start appropriate activity

--- sanshinclient/sanshinclient/src/main/java/jp/gr/java_conf/jyukon/sanshin/sanshinclient/EntryActivity.java
+++ sanshinclient/sanshinclient/src/main/java/jp/gr/java_conf/jyukon/sanshin/sanshinclient/EntryActivity.java
@@ -11,40 +11,43 @@ import android.util.Log;
 import com.android.future.usb.UsbAccessory;
 import com.google.code.sanshin.sanshinclient.openaccessory.OpenAccessory;
 import com.google.code.sanshin.sanshinclient.view.SanshinViewHardStringsHardPickUpImpl;
+import com.google.code.sanshin.sanshinclient.view.SanshinViewSoftStringsHardPickUpImpl;
 import com.google.code.sanshin.sanshinclient.view.SanshinViewSoftStringsSoftPickUpImpl;
 
 public class EntryActivity extends Activity {
     private static final String TAG = EntryActivity.class.getSimpleName();
 
-    static final String TARGET_PACKAGE = "com.google.code.sanshin.sanshinclient.view";
-    static final String TARGET_CLASS = "com.google.code.sanshin.sanshinclient.view.SanshinViewHardStringsHardPickUpImpl";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Intent intent;
+        String accessoryModel = null;
 
         super.onCreate(savedInstanceState);
-        Log.d(TAG, "onCreate");
 
         OpenAccessory openAccessory = RoboGuice.getInjector(getApplicationContext()).getInstance(
                 OpenAccessory.class);
         openAccessory.onCreate(this);
         UsbAccessory accessory = openAccessory.findAccessory();
+        if (accessory != null) {
+            accessoryModel = accessory.getModel();
+        }
         openAccessory.onDestroy();
 
-        if ("android.hardware.usb.action.USB_ACCESSORY_ATTACHED".equals(getIntent().getAction())
-                || accessory != null) {
-            // if (true) {
+        if ("Sanshin Custom".equals(accessoryModel)) {
             intent = new Intent(this, SanshinViewHardStringsHardPickUpImpl.class);
             Log.d(TAG, "onCreate: try to start SanshinViewHardStringsHardPickUpImpl");
+        } else if ("DemoKit".equals(accessoryModel)) {
+            intent = new Intent(this, SanshinViewSoftStringsHardPickUpImpl.class);
+            Log.d(TAG, "onCreate: try to start SanshinViewSoftStringsHardPickUpImpl");
         } else {
+            // Accessory is not attached or unknown accessory.
             intent = new Intent(this, SanshinViewSoftStringsSoftPickUpImpl.class);
             Log.d(TAG, "onCreate: try to start SanshinViewSoftStringsSoftPickUpImpl");
         }
         try {
             startActivity(intent);
         } catch (ActivityNotFoundException e) {
-            Log.e(TAG, "unable to start " + TARGET_CLASS + " activity", e);
+            Log.e(TAG, "unable to start activity", e);
         }
         finish();
     }
