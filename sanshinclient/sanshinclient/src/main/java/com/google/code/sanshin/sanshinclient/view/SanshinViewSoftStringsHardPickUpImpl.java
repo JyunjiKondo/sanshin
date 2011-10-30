264d9399c34cb80b7b6168fb9a217570480286da
Jyunji Kondo <jyukon@gmail.com>
2011-10-30 09:57:12 +0900

    add handling ImageButtons

--- sanshinclient/sanshinclient/src/main/java/com/google/code/sanshin/sanshinclient/view/SanshinViewSoftStringsHardPickUpImpl.java
+++ sanshinclient/sanshinclient/src/main/java/com/google/code/sanshin/sanshinclient/view/SanshinViewSoftStringsHardPickUpImpl.java
@@ -4,10 +4,14 @@ package com.google.code.sanshin.sanshinclient.view;
 import jp.gr.java_conf.jyukon.sanshin.sanshinclient.EntryActivity;
 import jp.gr.java_conf.jyukon.sanshin.sanshinclient.R;
 import roboguice.RoboGuice;
+import roboguice.inject.InjectView;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.google.code.sanshin.sanshinclient.openaccessory.AccessoryListener;
 import com.google.code.sanshin.sanshinclient.openaccessory.OpenAccessory;
@@ -17,11 +21,57 @@ public class SanshinViewSoftStringsHardPickUpImpl extends SanshinViewImpl implem
         AccessoryListener {
     private static final String TAG = SanshinViewSoftStringsHardPickUpImpl.class.getSimpleName();
     private static final int VELOCITY = 64;
+    private static final int MALE_OPEN_NOTE = 60; // C4
+    private static final int MALE_FRET1_NOTE = 62; // D4
+    private static final int MALE_FRET2_NOTE = 64; // E4
+    private static final int MIDDLE_OPEN_NOTE = 65; // F4
+    private static final int MIDDLE_FRET1_NOTE = 67; // G4
+    private static final int MIDDLE_FRET2_NOTE = 69; // A5
+    private static final int MIDDLE_FRET3_NOTE = 71; // B5
+    private static final int FEMALE_OPEN_NOTE = 72; // C5
+    private static final int FEMALE_FRET1_NOTE = 74; // D5
+    private static final int FEMALE_FRET2_NOTE = 76; // E5
+    private static final int FEMALE_FRET3_NOTE = 77; // F5
+    private static final int FEMALE_FRET4_NOTE = 79; // G5
+
+    @InjectView(R.id.imageButtonMaleOpen)
+    ImageButton imageButtonMaleOpen;
+    @InjectView(R.id.imageButtonMaleFret1)
+    ImageButton imageButtonMaleFret1;
+    @InjectView(R.id.imageButtonMaleFret2)
+    ImageButton imageButtonMaleFret2;
+    @InjectView(R.id.imageButtonMaleFret3)
+    ImageButton imageButtonMaleFret3;
+    @InjectView(R.id.imageButtonMaleFret4)
+    ImageButton imageButtonMaleFret4;
+
+    @InjectView(R.id.imageButtonMiddleOpen)
+    ImageButton imageButtonMiddleOpen;
+    @InjectView(R.id.imageButtonMiddleFret1)
+    ImageButton imageButtonMiddleFret1;
+    @InjectView(R.id.imageButtonMiddleFret2)
+    ImageButton imageButtonMiddleFret2;
+    @InjectView(R.id.imageButtonMiddleFret3)
+    ImageButton imageButtonMiddleFret3;
+    @InjectView(R.id.imageButtonMiddleFret4)
+    ImageButton imageButtonMiddleFret4;
+
+    @InjectView(R.id.imageButtonFemaleOpen)
+    ImageButton imageButtonFemaleOpen;
+    @InjectView(R.id.imageButtonFemaleFret1)
+    ImageButton imageButtonFemaleFret1;
+    @InjectView(R.id.imageButtonFemaleFret2)
+    ImageButton imageButtonFemaleFret2;
+    @InjectView(R.id.imageButtonFemaleFret3)
+    ImageButton imageButtonFemaleFret3;
+    @InjectView(R.id.imageButtonFemaleFret4)
+    ImageButton imageButtonFemaleFret4;
 
     private SoftStringsHardPickUpPresenter mPresenter;
 
     // @Inject
     // OpenAccessory mOpenAccessory;
+
     private OpenAccessory mOpenAccessory;
 
     public SanshinViewSoftStringsHardPickUpImpl() {
@@ -37,6 +87,10 @@ public class SanshinViewSoftStringsHardPickUpImpl extends SanshinViewImpl implem
         mOpenAccessory.onCreate(getApplicationContext());
         mOpenAccessory.addListener(this);
         setContentView(R.layout.soft_strings);
+
+        makeUnnecessaryImageButtonsInvisible();
+        setImageButtonListeners();
+        setInitialFingerPosition();
         Log.d(TAG, "onCreate");
     }
 
@@ -112,4 +166,124 @@ public class SanshinViewSoftStringsHardPickUpImpl extends SanshinViewImpl implem
         }
         finish();
     }
+
+    private void makeUnnecessaryImageButtonsInvisible() {
+        imageButtonMaleOpen.setVisibility(View.INVISIBLE);
+        imageButtonMiddleOpen.setVisibility(View.INVISIBLE);
+        imageButtonFemaleOpen.setVisibility(View.INVISIBLE);
+
+        imageButtonMaleFret3.setVisibility(View.INVISIBLE);
+        imageButtonMaleFret4.setVisibility(View.INVISIBLE);
+
+        imageButtonMiddleFret4.setVisibility(View.INVISIBLE);
+    }
+
+    private void setImageButtonListeners() {
+
+        imageButtonMaleFret1.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View view, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    mFingerPositionListener.onMaleStringFingerPositionChanged(MALE_FRET1_NOTE);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    mFingerPositionListener.onMaleStringFingerPositionChanged(MALE_OPEN_NOTE);
+                }
+                return false;
+            }
+        });
+
+        imageButtonMaleFret2.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View view, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    mFingerPositionListener.onMaleStringFingerPositionChanged(MALE_FRET2_NOTE);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    mFingerPositionListener.onMaleStringFingerPositionChanged(MALE_OPEN_NOTE);
+                }
+                return false;
+            }
+        });
+
+        imageButtonMiddleFret1.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View view, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    mFingerPositionListener.onMiddleStringFingerPositionChanged(MIDDLE_FRET1_NOTE);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    mFingerPositionListener.onMiddleStringFingerPositionChanged(MIDDLE_OPEN_NOTE);
+                }
+                return false;
+            }
+        });
+
+        imageButtonMiddleFret2.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View view, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    mFingerPositionListener.onMiddleStringFingerPositionChanged(MIDDLE_FRET2_NOTE);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    mFingerPositionListener.onMiddleStringFingerPositionChanged(MIDDLE_OPEN_NOTE);
+                }
+                return false;
+            }
+        });
+
+        imageButtonMiddleFret3.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View view, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    mFingerPositionListener.onMiddleStringFingerPositionChanged(MIDDLE_FRET3_NOTE);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    mFingerPositionListener.onMiddleStringFingerPositionChanged(MIDDLE_OPEN_NOTE);
+                }
+                return false;
+            }
+        });
+
+        imageButtonFemaleFret1.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View view, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    mFingerPositionListener.onFemaleStringFingerPositionChanged(FEMALE_FRET1_NOTE);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    mFingerPositionListener.onFemaleStringFingerPositionChanged(FEMALE_OPEN_NOTE);
+                }
+                return false;
+            }
+        });
+
+        imageButtonFemaleFret2.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View view, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    mFingerPositionListener.onFemaleStringFingerPositionChanged(FEMALE_FRET2_NOTE);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    mFingerPositionListener.onFemaleStringFingerPositionChanged(FEMALE_OPEN_NOTE);
+                }
+                return false;
+            }
+        });
+
+        imageButtonFemaleFret3.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View view, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    mFingerPositionListener.onFemaleStringFingerPositionChanged(FEMALE_FRET3_NOTE);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    mFingerPositionListener.onFemaleStringFingerPositionChanged(FEMALE_OPEN_NOTE);
+                }
+                return false;
+            }
+        });
+
+        imageButtonFemaleFret4.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View view, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    mFingerPositionListener.onFemaleStringFingerPositionChanged(FEMALE_FRET4_NOTE);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    mFingerPositionListener.onFemaleStringFingerPositionChanged(FEMALE_OPEN_NOTE);
+                }
+                return false;
+            }
+        });
+    }
+
+    private void setInitialFingerPosition() {
+        mFingerPositionListener.onMaleStringFingerPositionChanged(MALE_OPEN_NOTE);
+        mFingerPositionListener.onMiddleStringFingerPositionChanged(MIDDLE_OPEN_NOTE);
+        mFingerPositionListener.onFemaleStringFingerPositionChanged(FEMALE_OPEN_NOTE);
+    }
+
 }
